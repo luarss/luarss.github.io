fbddd673ca6d4719a0bcd509072c7c1c6870c9f0
Jack Luar <39641663+luarss@users.noreply.github.com>
2025-11-21 15:55:16 +0000

    fix flex scaling in experience card
    Signed-off-by: Jack Luar <39641663+luarss@users.noreply.github.com>

--- src/components/experienceCard/ExperienceCard.js
+++ src/components/experienceCard/ExperienceCard.js
@@ -45,14 +45,14 @@ class ExperienceCard extends Component {
           )}
         </div>
         <Fade right duration={2000} distance="40px">
-          <div style={{ display: "flex", flexDirection: "row" }}>
+          <div style={{ display: "flex", flexDirection: "row", flex: 1 }}>
             <div
               className="arrow-left"
               style={{ borderRight: `10px solid ${theme.body}` }}
             ></div>
             <div
               className="experience-card"
-              style={{ background: `${theme.body}` }}
+              style={{ background: `${theme.body}`, flex: 1 }}
             >
               <div
                 style={{
